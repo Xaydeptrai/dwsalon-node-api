d9b84847ef65ab7a6e8e812eb0766193aea41acc
xaytr <xaytrong2002@gmail.com>
2024-08-30 15:10:37 +0700

    ensure loadUserByEmailByRepository return with id and password only

--- src/infrastructure/repositories/load-user-by-email-repository.spec.js
+++ src/infrastructure/repositories/load-user-by-email-repository.spec.js
@@ -7,7 +7,13 @@ class LoadUserByEmailRepository {
   }
 
   async load (email) {
-    const user = await this.userModel.findOne({ email })
+    const user = await this.userModel.findOne({
+      email
+    }, {
+      projection: {
+        password: 1
+      }
+    })
     return user
   }
 }
@@ -23,10 +29,7 @@ const makeSut = () => {
 
 describe('LoadUserByEmail Repository', () => {
   beforeAll(async () => {
-    client = await MongoClient.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    client = await MongoClient.connect(process.env.MONGO_URL)
     db = client.db()
   })
 
@@ -46,10 +49,17 @@ describe('LoadUserByEmail Repository', () => {
 
   test('Should return an user if user is found', async () => {
     const { sut, userModel } = makeSut()
-    await userModel.insertOne({
-      email: 'valid_email@mail.com'
+    const fakeUser = await userModel.insertOne({
+      email: 'valid_email@mail.com',
+      name: 'any_name',
+      age: 50,
+      state: 'any_state',
+      password: 'hashed_password'
     })
     const user = await sut.load('valid_email@mail.com')
-    expect(user.email).toBe('valid_email@mail.com')
+    expect(user).toEqual({
+      _id: fakeUser.insertedId,
+      password: 'hashed_password'
+    })
   })
 })
